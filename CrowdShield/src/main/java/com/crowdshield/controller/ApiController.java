bbcf410aa96e3b215affe439fc491a31aefbb256
Ghansham Pravin Bordekar <ghanshamzzx@gmail.com>
2025-10-07 21:21:15 +0530

    Edit File

--- CrowdShield/src/main/java/com/crowdshield/controller/ApiController.java
+++ CrowdShield/src/main/java/com/crowdshield/controller/ApiController.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://crowd-shield-ai-powered-crowd-disas-delta.vercel.app")
+@CrossOrigin(origins = "https://crowd-shield-ai-powered-crowd-disas-xi.vercel.app/")
 @RequestMapping("/api")
 public class ApiController {
 
--- CrowdShield/src/main/java/com/crowdshield/controller/SmsController.java
+++ CrowdShield/src/main/java/com/crowdshield/controller/SmsController.java
@@ -13,7 +13,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 @RestController
-@CrossOrigin(origins = "https://crowd-shield-ai-powered-crowd-disas-delta.vercel.app")
+@CrossOrigin(origins = "https://crowd-shield-ai-powered-crowd-disas-xi.vercel.app/")
 @RequestMapping("/api/sms")
 public class SmsController {
 
