f8247034b59b43ebb351e2c39aa7ed5facebd842
Ghansham Pravin Bordekar <ghanshamzzx@gmail.com>
2025-10-11 13:04:08 +0530

    Edit File

--- CrowdShield/src/main/java/com/crowdshield/controller/UserController.java
+++ CrowdShield/src/main/java/com/crowdshield/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "https://crowd-shield-ai-powered-crowd-disas-ruddy.vercel.app/")
+@CrossOrigin(origins = "https://crowd-shield-ai-powered-crowd-disas-tan.vercel.app/")
 @RequestMapping("/api")
 public class UserController {
 
